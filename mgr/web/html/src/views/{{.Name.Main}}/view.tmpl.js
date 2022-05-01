7c1284588ca986cd3fd6596205f62e4787c15bae
15828680877 <lib4net@163.com>
2022-05-01 11:06:04 +0800

    添加日期数字格式化函数

---
+++ mgr/web/html/src/views/{{.Name.Main}}/view.tmpl.js
@@ -0,0 +1,24 @@
+{{- $table :=. -}}
+{{- $vrow := $table.VRows -}}
+ show(form) {
+    this.conf.visible = true
+    let that = this;
+    this.$theia.http
+      .get("/{{.Name.CName|lower}}",form)
+      .then((res) => {
+        Object.assign(that.view, res)
+    {{- range $i,$c := $vrow -}}
+      {{- if eq true $c.Ext.IsSelect}}
+        that.view.{{$c.Name}}_label = that.$theia.enum.getName("{{$c.Ext.SLType}}",res.{{$c.Name}})
+     {{- end -}}
+      {{- end}}
+      })
+      .catch((res) => {
+        let code = res.response.status;
+        let msg = `{{.Desc}}查询失败(${code})`;
+        that.$notify.error({ title: "失败", message: msg, duration: 5000 });
+      });
+  },
+   colorful(r){
+     return this.$theia.env.conf.colorful[r]
+  },
