6e492e74e31f30027804dfa5546785ba9d45a35c
15828680877 <lib4net@163.com>
2022-05-04 21:03:39 +0800

    添加文件

--- mgr/project/glob.go
+++ mgr/project/glob.go
@@ -5,11 +5,13 @@ import "github.com/micro-plat/hycli/md"
 type Project struct {
 	Name    string
 	PkgName string
+	Dot     string
 }
 
 func NewProject(name string) *Project {
 	return &Project{
 		Name:    name,
 		PkgName: md.GetPkgName(),
+		Dot:     ".",
 	}
 }
