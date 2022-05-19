5e61ebe7606cac0d986003346d5abd29911ada3d
15828680877 <lib4net@163.com>
2022-05-19 17:51:11 +0800

    添加移动端代码

---
+++ mob/main.go
@@ -0,0 +1,63 @@
+package mgr
+
+import (
+	"github.com/lib4dev/cli/cmds"
+	"github.com/micro-plat/hycli/mgr/api"
+	"github.com/micro-plat/hycli/mob/project"
+	"github.com/micro-plat/hycli/mob/web"
+	"github.com/urfave/cli"
+)
+
+func init() {
+	cmds.Register(
+		cli.Command{
+			Name:  "mob",
+			Usage: "创建vue前端项目",
+			Subcommands: cli.Commands{
+				{
+					Name:   "create",
+					Usage:  "创建项目",
+					Action: project.CreateByCtx,
+					Flags: []cli.Flag{
+						cli.StringFlag{Name: "docs,c", Usage: `-创建docs目录`},
+					},
+				},
+				{
+					Name:   "code",
+					Usage:  "创建web页面、后端服务代码",
+					Action: CreateCodeByCtx,
+					Flags: []cli.Flag{
+						cli.StringFlag{Name: "table,t", Usage: `-表名称`},
+						cli.BoolFlag{Name: "cover,v", Usage: `-文件已存在时自动覆盖`},
+					},
+				},
+				{
+					Name:   "web",
+					Usage:  "创建web页面",
+					Action: web.CreatePageByCtx,
+					Flags: []cli.Flag{
+						cli.StringFlag{Name: "table,t", Usage: `-表名称`},
+						cli.BoolFlag{Name: "cover,v", Usage: `-文件已存在时自动覆盖`},
+					},
+				},
+				{
+					Name:   "api",
+					Usage:  "创建api服务代码",
+					Action: api.CreateServiceByCtx,
+					Flags: []cli.Flag{
+						cli.StringFlag{Name: "table,t", Usage: `-表名称`},
+						cli.BoolFlag{Name: "cover,v", Usage: `-文件已存在时自动覆盖`},
+					},
+				},
+				{
+					Name:   "enums",
+					Usage:  "创建enums服务代码",
+					Action: api.CreateEnumsByCtx,
+					Flags: []cli.Flag{
+						cli.StringFlag{Name: "table,t", Usage: `-表名称`},
+						cli.BoolFlag{Name: "cover,v", Usage: `-文件已存在时自动覆盖`},
+					},
+				},
+			},
+		})
+}
