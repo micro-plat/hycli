43a3e30c4481abfe0a3b6336536501e598a44c3c
15828680877 <lib4net@163.com>
2023-03-13 21:52:49 +0800

    修改模板

---
+++ data/data_test.go
@@ -0,0 +1,23 @@
+package data
+
+import (
+	"testing"
+
+	"github.com/micro-plat/hycli/data/internal/md"
+	"github.com/micro-plat/lib4go/assert"
+)
+
+func TestV(t *testing.T) {
+	k := "tp(cut,le,4)"
+	c, p, v := md.GetConsByTag("tp", k)
+	assert.Equal(t, "cut", c)
+	assert.Equal(t, "le", p)
+	assert.Equal(t, "4", v)
+}
+
+func TestColor(t *testing.T) {
+	c, p, v :=md.GetConsByTagIgnorecase("color","color")
+	assert.Equal(t, "color",c)
+	assert.Equal(t,"",p)
+	assert.Equal(t,"",v)
+}
