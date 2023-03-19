c984ebeceeb5255b8ab0bb5d15cc666e1e0a3caf
15828680877 <lib4net@163.com>
2023-03-19 13:05:53 +0800

    重构

---
+++ mgr/web/html/src/views/{{.Name.Prefix}}/{{.Name.Main}}/listbar.tmpl.js
@@ -0,0 +1,9 @@
+{{$table :=.}}
+{{ range $i,$c:= $table.LStatOpts }}
+{{- $ucols := fltrColums $table $c.RwName -}}
+stat_{{$c.UNQ}}:{
+    {{range $j,$m:= $ucols -}}
+    {{$m.Name}}:0,
+    {{- end}}
+}
+{{end}}
