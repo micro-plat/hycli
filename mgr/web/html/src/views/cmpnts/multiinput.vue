a8c6c53b14e63f3eabe1e8636e5c925fd07a97d0
15828680877 <lib4net@163.com>
2023-04-19 08:50:00 +0800

    支持添加、编辑、删除通过组件配置

---
+++ mgr/web/html/src/views/cmpnts/multiinput.vue
@@ -0,0 +1,62 @@
+<template>
+    <div>
+        <el-tag v-for="tag in tags" :key="tag" class="mx-1" closable :disable-transitions="false" @close="handleClose(tag)">
+            {{ tag }}
+        </el-tag>
+        <el-input v-if="inputVisible" ref="input" v-model="inputValue" class="ml-1 w-20" size="small"
+            @keyup.enter="handleInputConfirm" @blur="handleInputConfirm" />
+        <el-button v-else class="button-new-tag ml-1" size="small" @click="showInput">
+            + 添加
+        </el-button>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        tags: {
+            type: Array,
+            default: []
+        }
+    },
+    data() {
+        return {
+            inputValue: "",
+            inputVisible: false,
+        }
+    },
+    methods: {
+        showInput() {
+            this.inputVisible = true
+            this.$nextTick(() => {
+                this.$refs.input.focus()
+            })
+        },
+
+        handleClose(tag) {
+            this.tags.splice(this.tags.indexOf(tag), 1)
+        },
+        handleInputConfirm() {
+            let that = this
+            if (this.inputValue) {
+                // this.$theia.http.xget(this.inputValue, {}).then(res => {
+                //     var matches = data.match(/<title>(.*?)<\/title>/);
+                //     if ((matches || []).length > 0) {
+                //         that.tags.push(matches[0])
+                //         return
+                //     }
+                //     that.tags.push(that.inputValue)
+                // }).catch(err => {
+                //     that.tags.push(that.inputValue)
+                // });
+                that.tags.push(that.inputValue)
+            }
+            this.inputVisible = false
+            this.inputValue = ''
+
+
+        }
+    },
+
+}
+</script>
+<style scoped></style>
